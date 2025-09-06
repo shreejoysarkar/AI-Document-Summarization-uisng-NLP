b516d1c0a61e5815a543e5186a98d644410a6782
shreejoy <sarkarshreejoy30@gmail.com>
2025-09-06 11:22:48 +0530

    setup and requirements installed

--- setup.py
+++ setup.py
@@ -1,1 +1,30 @@
 # setup.py
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "AI-Document-Summarization-using-NLP"
+AUTHOR_USER_NAME = "shreejoysarkar"
+SRC_REPO = "DocSummarizer"
+AUTHOR_EMAIL = "sarkarshreejoy30@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A python package for document summarization using NLP",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+)
